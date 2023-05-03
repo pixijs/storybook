936044e1a0edda202fb4e096caecce63062059c2
GregVGW <greg.lahaye@vgw.co>
2023-05-03 11:38:48 +1000

    update name

--- packages/storybook-preset-vite/src/index.ts
+++ packages/storybook-preset-vite/src/index.ts
@@ -2,6 +2,6 @@ import type { StorybookConfigVite } from "./types";
 
 export * from "./types";
 
-export const webpack: StorybookConfigVite["viteFinal"] = (config) => {
+export const vite: StorybookConfigVite["viteFinal"] = (config) => {
   return config;
 };
